f498eb8ea4944a9466a9ee95ff65b24f63a8ef72
Aleksey Ostrikov <aleksey.ostrikov@instamart.ru>
2021-04-03 13:38:41 +0200

    rules 2

--- src/navigation/ProfileNavigator.jsx
+++ src/navigation/ProfileNavigator.jsx
@@ -1,161 +1,170 @@
 import React from 'react';
-import { createStackNavigator, CardStyleInterpolators } from '@react-navigation/stack';
+import {createStackNavigator, CardStyleInterpolators} from '@react-navigation/stack';
 import ProfileScreen from '../screens/ProfileScreen';
 import Header from '../components/Header';
-import { TouchableOpacity } from 'react-native';
+import {TouchableOpacity} from 'react-native';
 import EStyleSheet from 'react-native-extended-stylesheet';
 import Icon from 'react-native-vector-icons/MaterialIcons';
 import Ionic from 'react-native-vector-icons/MaterialCommunityIcons';
 import ProfileSettingsScreen from '../screens/ProfileSettingsScreen';
 import EditProfileScreen from '../screens/EditProfileScreen';
-import { useNavigation } from '@react-navigation/native';
+import {useNavigation} from '@react-navigation/native';
 import ChangePassScreen from '../screens/ChangePassScreen';
 import AboutAppScreen from '../screens/AboutAppScreen';
 import RulesScreen from '../screens/RulesScreen';
-import { connect } from 'react-redux';
+import {connect} from 'react-redux';
 import CreateTeamNavigator from './CreateTeamNavigator';
 import CreateTeamSuccessfullyScreen from '../screens/CreateTeamSuccessfullyScreen';
 import LeaveTheTeamScreen from '../screens/LeaveTheTeamScreen';
 import LeaveTheTeamConfirmationScreen from '../screens/LeaveTheTeamConfirmationScreen';
+import RulesScreenSecond from "../screens/RulesScreenSecond";
 
 const Stack = createStackNavigator();
 
 const styles = EStyleSheet.create({
-	button: {
-		marginRight: '10rem',
-	},
-	$settings: '35rem',
-	$close: '35rem',
+    button: {
+        marginRight: '10rem',
+    },
+    $settings: '35rem',
+    $close: '35rem',
 });
 
 const SettingButton = () => {
-	const navigation = useNavigation();
-	return (
-		<TouchableOpacity
-			activeOpacity={0.7}
-			onPress={() => navigation.navigate('ProfileSettings')}
-			style={styles.button}
-		>
-			<Icon
-				color={'white'}
-				name={'settings'}
-				size={styles.$settings}/>
-		</TouchableOpacity>
-	);
+    const navigation = useNavigation();
+    return (
+        <TouchableOpacity
+            activeOpacity={0.7}
+            onPress={() => navigation.navigate('ProfileSettings')}
+            style={styles.button}
+        >
+            <Icon
+                color={'white'}
+                name={'settings'}
+                size={styles.$settings}/>
+        </TouchableOpacity>
+    );
 };
 
 const CloseButton = () => {
-	const navigation = useNavigation();
-	return (
-		<TouchableOpacity
-			activeOpacity={0.7}
-			onPress={() => navigation.navigate('Profile')}
-			style={styles.button}
-		>
-			<Ionic
-				color={'white'}
-				name={'close-circle'}
-				size={styles.$close}/>
-		</TouchableOpacity>
-	);
+    const navigation = useNavigation();
+    return (
+        <TouchableOpacity
+            activeOpacity={0.7}
+            onPress={() => navigation.navigate('Profile')}
+            style={styles.button}
+        >
+            <Ionic
+                color={'white'}
+                name={'close-circle'}
+                size={styles.$close}/>
+        </TouchableOpacity>
+    );
 };
 
 const ProfileNavigator = (props) => {
-	if (props.profileType === 'COMMAND' && !props.teamId) {
-		return <CreateTeamNavigator/>;
-	}
+    if (props.profileType === 'COMMAND' && !props.teamId) {
+        return <CreateTeamNavigator/>;
+    }
 
-	return (
-		<Stack.Navigator
-			initialRouteName='Profile'
-			headerMode='float'
-			screenOptions={{
-				...Header,
-				headerStyle: {
-					...Header.headerStyle,
-					borderBottomWidth: 1,
-					borderColor: 'rgba(255, 255, 255, 0.75)',
-				},
-			}}
-		>
-			<Stack.Screen
-				name='Profile'
-				options={{
-					title: props.profileType === 'ONE' ? 'Профиль игрока' : 'Профиль команды',
-					cardStyleInterpolator: CardStyleInterpolators.forHorizontalIOS,
-					headerRight: () => <SettingButton/>,
-				}}
-				component={ProfileScreen}/>
-			<Stack.Screen
-				name='ProfileSettings'
-				options={{
-					title: 'Настройки',
-					cardStyleInterpolator: CardStyleInterpolators.forVerticalIOS,
-					headerRight: () => <CloseButton/>,
-				}}
-				component={ProfileSettingsScreen}/>
-			<Stack.Screen
-				name='EditProfile'
-				options={{
-					title: 'Редактирование',
-					cardStyleInterpolator: CardStyleInterpolators.forHorizontalIOS,
-					headerRight: () => <CloseButton/>,
-				}}
-				component={EditProfileScreen}/>
-			<Stack.Screen
-				name='ChangePass'
-				options={{
-					title: 'Изменение пароля',
-					cardStyleInterpolator: CardStyleInterpolators.forHorizontalIOS,
-					headerRight: () => <CloseButton/>,
-				}}
-				component={ChangePassScreen}/>
-			<Stack.Screen
-				name='AboutApp'
-				options={{
-					title: 'Политика',
-					cardStyleInterpolator: CardStyleInterpolators.forHorizontalIOS,
-					headerRight: () => <CloseButton/>,
-				}}
-				component={AboutAppScreen}/>
-			<Stack.Screen
-				name='Rules'
-				options={{
-					title: 'Правила',
-					cardStyleInterpolator: CardStyleInterpolators.forHorizontalIOS,
-					headerRight: () => <CloseButton/>,
-				}}
-				component={RulesScreen}/>
-			<Stack.Screen
-				name='CreateTeamSuccessfully'
-				options={{
-					title: 'Создание команды',
-					cardStyleInterpolator: CardStyleInterpolators.forHorizontalIOS,
-				}}
-				component={CreateTeamSuccessfullyScreen}/>
-			<Stack.Screen
-				name='LeaveTheTeam'
-				options={{
-					title: 'Покинуть команду',
-					cardStyleInterpolator: CardStyleInterpolators.forHorizontalIOS,
-					headerRight: () => <CloseButton/>,
-				}}
-				component={LeaveTheTeamScreen}/>
-			<Stack.Screen
-				name='LeaveTheTeamConfirmation'
-				options={{
-					title: 'Покинуть команду',
-					cardStyleInterpolator: CardStyleInterpolators.forVerticalIOS,
-					headerRight: () => <CloseButton/>,
-				}}
-				component={LeaveTheTeamConfirmationScreen}/>
-		</Stack.Navigator>
-	);
+    return (
+        <Stack.Navigator
+            initialRouteName='Profile'
+            headerMode='float'
+            screenOptions={{
+                ...Header,
+                headerStyle: {
+                    ...Header.headerStyle,
+                    borderBottomWidth: 1,
+                    borderColor: 'rgba(255, 255, 255, 0.75)',
+                },
+            }}
+        >
+            <Stack.Screen
+                name='Profile'
+                options={{
+                    title: props.profileType === 'ONE' ? 'Профиль игрока' : 'Профиль команды',
+                    cardStyleInterpolator: CardStyleInterpolators.forHorizontalIOS,
+                    headerRight: () => <SettingButton/>,
+                }}
+                component={ProfileScreen}/>
+            <Stack.Screen
+                name='ProfileSettings'
+                options={{
+                    title: 'Настройки',
+                    cardStyleInterpolator: CardStyleInterpolators.forVerticalIOS,
+                    headerRight: () => <CloseButton/>,
+                }}
+                component={ProfileSettingsScreen}/>
+            <Stack.Screen
+                name='EditProfile'
+                options={{
+                    title: 'Редактирование',
+                    cardStyleInterpolator: CardStyleInterpolators.forHorizontalIOS,
+                    headerRight: () => <CloseButton/>,
+                }}
+                component={EditProfileScreen}/>
+            <Stack.Screen
+                name='ChangePass'
+                options={{
+                    title: 'Изменение пароля',
+                    cardStyleInterpolator: CardStyleInterpolators.forHorizontalIOS,
+                    headerRight: () => <CloseButton/>,
+                }}
+                component={ChangePassScreen}/>
+            <Stack.Screen
+                name='AboutApp'
+                options={{
+                    title: 'Политика',
+                    cardStyleInterpolator: CardStyleInterpolators.forHorizontalIOS,
+                    headerRight: () => <CloseButton/>,
+                }}
+                component={AboutAppScreen}/>
+            <Stack.Screen
+                name='Rules'
+                options={{
+                    title: 'Правила',
+                    cardStyleInterpolator: CardStyleInterpolators.forHorizontalIOS,
+                    headerRight: () => <CloseButton/>,
+                }}
+                component={RulesScreen}/>
+            <Stack.Screen
+                name='RulesSecond'
+                options={{
+                    title: 'Правила',
+                    cardStyleInterpolator: CardStyleInterpolators.forHorizontalIOS,
+                    headerRight: () => <CloseButton/>,
+                }}
+                component={RulesScreenSecond}/>
+            <Stack.Screen
+                name='CreateTeamSuccessfully'
+                options={{
+                    title: 'Создание команды',
+                    cardStyleInterpolator: CardStyleInterpolators.forHorizontalIOS,
+                }}
+                component={CreateTeamSuccessfullyScreen}/>
+            <Stack.Screen
+                name='LeaveTheTeam'
+                options={{
+                    title: 'Покинуть команду',
+                    cardStyleInterpolator: CardStyleInterpolators.forHorizontalIOS,
+                    headerRight: () => <CloseButton/>,
+                }}
+                component={LeaveTheTeamScreen}/>
+            <Stack.Screen
+                name='LeaveTheTeamConfirmation'
+                options={{
+                    title: 'Покинуть команду',
+                    cardStyleInterpolator: CardStyleInterpolators.forVerticalIOS,
+                    headerRight: () => <CloseButton/>,
+                }}
+                component={LeaveTheTeamConfirmationScreen}/>
+        </Stack.Navigator>
+    );
 };
 
 const mapStateToProps = state => ({
-	profileType: state.profile.profileType,
-	teamId: state.profile.team.ID,
+    profileType: state.profile.profileType,
+    teamId: state.profile.team.ID,
 });
 
 export default connect(mapStateToProps, null)(ProfileNavigator);
--- src/screens/ProfileSettingsScreen.jsx
+++ src/screens/ProfileSettingsScreen.jsx
@@ -1,166 +1,174 @@
 import React from 'react';
 import ScreenWrapper from './ScreenWrapper';
 import Button from '../components/Button';
-import { useNavigation } from '@react-navigation/native';
-import { ScrollView, Text, TouchableOpacity, View } from 'react-native';
+import {useNavigation} from '@react-navigation/native';
+import {ScrollView, Text, TouchableOpacity, View} from 'react-native';
 import EStyleSheet from 'react-native-extended-stylesheet';
-import { bindActionCreators } from 'redux';
-import { changeProfileType, signOut } from '../store/actions/profile';
-import { connect } from 'react-redux';
+import {bindActionCreators} from 'redux';
+import {changeProfileType, signOut} from '../store/actions/profile';
+import {connect} from 'react-redux';
 import Icon from 'react-native-vector-icons/FontAwesome';
 
 const userMenu = [
-	{
-		title: 'Редактировать профиль игрока',
-		targetScreen: 'EditProfile',
-	},
-	{
-		title: 'Сменить пароль',
-		targetScreen: 'ChangePass',
-	},
-	{
-		title: 'Правила',
-		targetScreen: 'Rules',
-	},
-	{
-		title: 'Политика',
-		targetScreen: 'AboutApp',
-	},
-	{
-		title: 'Присоединиться к команде',
-		targetScreen: 'Scanner',
-	},
-	{
-		title: 'Покинуть команду',
-		targetScreen: 'LeaveTheTeam',
-	},
+    {
+        title: 'Редактировать профиль игрока',
+        targetScreen: 'EditProfile',
+    },
+    {
+        title: 'Сменить пароль',
+        targetScreen: 'ChangePass',
+    },
+    {
+        title: 'Правила проведения турниров',
+        targetScreen: 'Rules',
+    },
+    {
+        title: 'Правила пользования приложением',
+        targetScreen: 'RulesSecond',
+    },
+    {
+        title: 'Политика конфиденциальности',
+        targetScreen: 'AboutApp',
+    },
+    {
+        title: 'Присоединиться к команде',
+        targetScreen: 'Scanner',
+    },
+    {
+        title: 'Покинуть команду',
+        targetScreen: 'LeaveTheTeam',
+    },
 ];
 
 const teamMenu = [
-	{
-		title: 'Редактировать профиль команды',
-		targetScreen: 'EditProfile',
-	},
-	{
-		title: 'Правила',
-		targetScreen: 'Rules',
-	},
-	{
-		title: 'Политика',
-		targetScreen: 'AboutApp',
-	},
+    {
+        title: 'Редактировать профиль команды',
+        targetScreen: 'EditProfile',
+    },
+    {
+        title: 'Правила проведения турниров',
+        targetScreen: 'Rules',
+    },
+    {
+        title: 'Правила пользования приложением',
+        targetScreen: 'RulesSecond',
+    },
+    {
+        title: 'Политика конфиденциальности',
+        targetScreen: 'AboutApp',
+    },
 ];
 
 const styles = EStyleSheet.create({
-	page: {
-		flex: 1,
-		paddingHorizontal: '10rem',
-		paddingVertical: '20rem',
-		flexDirection: 'column',
-		justifyContent: 'flex-start',
-	},
-	item: {
-		flexDirection: 'row',
-		alignItems: 'center',
-		justifyContent: 'space-between',
-		paddingVertical: '10rem',
-	},
-	title: {
-		color: 'white',
-		fontSize: '11rem',
-		lineHeight: '13rem',
-		textTransform: 'uppercase',
-		flex: 1,
-		flexWrap: 'wrap',
-	},
-	$size: '35rem',
-	menu: {
-		paddingHorizontal: '10rem',
-		paddingBottom: '20rem',
-		flexGrow: 1,
-	},
+    page: {
+        flex: 1,
+        paddingHorizontal: '10rem',
+        paddingVertical: '20rem',
+        flexDirection: 'column',
+        justifyContent: 'flex-start',
+    },
+    item: {
+        flexDirection: 'row',
+        alignItems: 'center',
+        justifyContent: 'space-between',
+        paddingVertical: '10rem',
+    },
+    title: {
+        color: 'white',
+        fontSize: '11rem',
+        lineHeight: '13rem',
+        textTransform: 'uppercase',
+        flex: 1,
+        flexWrap: 'wrap',
+    },
+    $size: '35rem',
+    menu: {
+        paddingHorizontal: '10rem',
+        paddingBottom: '20rem',
+        flexGrow: 1,
+    },
 });
 
 const ProfileSettingsScreen = (props) => {
-	const navigation = useNavigation();
-	const renderItem = (item, index) => (
-		<TouchableOpacity
-			key={index}
-			activeOpacity={0.6}
-			style={styles.item}
-			onPress={() => !!item.targetScreen ? navigation.navigate(item.targetScreen) : null}
-		>
-			<View style={{flexGrow: 1, marginRight: 10}}>
-				<View style={{flexDirection: 'row'}}>
-					<Text style={styles.title}>{item.title}</Text>
-				</View>
-			</View>
-			<Icon
-				name="angle-right"
-				size={styles.$size}
-				color={'white'}
-			/>
-		</TouchableOpacity>
-	);
+    const navigation = useNavigation();
+    const renderItem = (item, index) => (
+        <TouchableOpacity
+            key={index}
+            activeOpacity={0.6}
+            style={styles.item}
+            onPress={() => !!item.targetScreen ? navigation.navigate(item.targetScreen) : null}
+        >
+            <View style={{flexGrow: 1, marginRight: 10}}>
+                <View style={{flexDirection: 'row'}}>
+                    <Text style={styles.title}>{item.title}</Text>
+                </View>
+            </View>
+            <Icon
+                name="angle-right"
+                size={styles.$size}
+                color={'white'}
+            />
+        </TouchableOpacity>
+    );
 
-	const changeProfileType = () => {
-		props.changeProfileType();
-		navigation.navigate('Profile');
-	};
+    const changeProfileType = () => {
+        props.changeProfileType();
+        navigation.navigate('Profile');
+    };
 
-	const menu = props.isTeamProfile ? teamMenu : userMenu;
+    const menu = props.isTeamProfile ? teamMenu : userMenu;
 
-	return (
-		<ScreenWrapper>
-			<View style={styles.page}>
-				<ScrollView
-					contentContainerStyle={styles.menu}>
-					{menu.map(renderItem)}
-					<TouchableOpacity
-						activeOpacity={0.6}
-						style={styles.item}
-						onPress={changeProfileType}
-					>
-						<View style={{flexGrow: 1, marginRight: 10}}>
-							<View style={{flexDirection: 'row'}}>
-								<Text style={styles.title}>
-									{props.isTeamProfile ?
-										'Переключиться на профиль игрока' :
-										'Переключиться на профиль команды'
-									}
-								</Text>
-							</View>
-						</View>
-						<Icon
-							name="angle-right"
-							size={styles.$size}
-							color={'white'}
-						/>
-					</TouchableOpacity>
-				</ScrollView>
-				<Button
-					onPress={() => props.signOut()}
-					title={'Выйти из приложения'}
-				/>
-			</View>
-		</ScreenWrapper>
-	);
+    return (
+        <ScreenWrapper>
+            <View style={styles.page}>
+                <ScrollView
+                    contentContainerStyle={styles.menu}>
+                    {menu.map(renderItem)}
+                    <TouchableOpacity
+                        activeOpacity={0.6}
+                        style={styles.item}
+                        onPress={changeProfileType}
+                    >
+                        <View style={{flexGrow: 1, marginRight: 10}}>
+                            <View style={{flexDirection: 'row'}}>
+                                <Text style={styles.title}>
+                                    {props.isTeamProfile ?
+                                        'Переключиться на профиль игрока' :
+                                        'Переключиться на профиль команды'
+                                    }
+                                </Text>
+                            </View>
+                        </View>
+                        <Icon
+                            name="angle-right"
+                            size={styles.$size}
+                            color={'white'}
+                        />
+                    </TouchableOpacity>
+                </ScrollView>
+                <Button
+                    onPress={() => props.signOut()}
+                    title={'Выйти из приложения'}
+                />
+            </View>
+        </ScreenWrapper>
+    );
 };
 
 const mapDispatchToProps = dispatch => {
-	return bindActionCreators({
-			signOut,
-			changeProfileType,
-		},
-		dispatch);
+    return bindActionCreators({
+            signOut,
+            changeProfileType,
+        },
+        dispatch);
 };
 
 const mapStateToProps = state => {
-	const {profile: {profileType}} = state;
-	const isTeamProfile = profileType === 'COMMAND';
-	return {
-		isTeamProfile,
-	};
+    const {profile: {profileType}} = state;
+    const isTeamProfile = profileType === 'COMMAND';
+    return {
+        isTeamProfile,
+    };
 };
 
 export default connect(mapStateToProps, mapDispatchToProps)(ProfileSettingsScreen);
